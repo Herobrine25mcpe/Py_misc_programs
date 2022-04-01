7a29e3907bd69407cb9bb2b41f0dd2cfb0b2f6fb
Abhay Tomer <69842633+Herobrine25mcpe@users.noreply.github.com>
2022-04-01 23:35:41 +0530

    Add files via upload

---
+++ binarySearch.py
@@ -0,0 +1,34 @@
+
+
+
+def binarySearch(array, key, low, high):
+
+    if high >= low:
+
+        mid = low + (high - low)//2
+
+        
+        if array[mid] == key:
+            return mid
+
+        
+        elif array[mid] > key:
+            return binarySearch(array, key, low, mid-1)
+
+        
+        else:
+            return binarySearch(array, key, mid + 1, high)
+
+    else:
+        return -1
+
+
+array = [3, 4, 5, 6, 7, 8, 9]
+key = 4
+
+result = binarySearch(array, key, 0, len(array)-1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
